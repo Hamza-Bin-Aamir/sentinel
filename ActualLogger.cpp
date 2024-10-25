678b124b38983c497c58c95858fe0bb03b678cb1
AzeemLiaqat <azeemliaqat78@gmail.com>
2024-10-25 20:19:58 +0500

    Getting Started

---
+++ ActualLogger.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+struct Coordinates{
+    double latitude;
+    double longitude;
+    double altitude;
+}
