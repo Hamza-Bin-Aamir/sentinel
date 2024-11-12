becf2f5cf02d100f23a3eb560f6931dcb787a3aa
SAreebZaheergit config --global user.name SAreebZaheer <sareebzaheer@gmail.com>
2024-11-12 19:53:00 +0500

    changed functions to accomodate graph part1

--- ControlSystems/ControlSurfaces/gridfins.hpp
+++ ControlSystems/ControlSurfaces/gridfins.hpp
@@ -90,64 +90,56 @@ public:
     }
 };
 
-void gkeepvertical(gridfin g[], int gnum, float pitch, float yaw)
+void gkeepvertical(gridSystem &graph, float pitch, float yaw)
 {
-    if (pitch > 0) // Nose Up
+    for (int i = 0; i < graph.numV; i++)
     {
-        for (int i = 0; i < gnum; i++)
+        for (auto &g : graph.adj[i])
         {
-            if (g[i].mount.front == 1)
-            {
-                g[i].angle = ((pitch * 60) / 360);
+            if (pitch > 0)
+            { // Nose Up
+                if (g.mount.front == 1)
+                {
+                    g.angle = ((pitch * 60) / 360);
+                }
+                else
+                {
+                    g.angle = -1 * ((pitch * 60) / 360);
+                }
             }
-            if (g[i].mount.front == 0)
-            {
-                g[i].angle = -1 * ((pitch * 60) / 360);
+            else if (pitch < 0)
+            { // Nose Down
+                if (g.mount.front == 1)
+                {
+                    g.angle = -1 * ((pitch * 60) / 360);
+                }
+                else
+                {
+                    g.angle = ((pitch * 60) / 360);
+                }
             }
-        }
-    }
 
-    if (pitch < 0) // Nose Down
-    {
-        for (int i = 0; i < gnum; i++)
-        {
-            if (g[i].mount.front == 1)
-            {
-                g[i].angle = -1 * ((pitch * 60) / 360);
+            if (yaw > 0)
+            { // Veer Right
+                if (g.mount.left == 1)
+                {
+                    g.angle += ((yaw * 60) / 360);
+                }
+                else
+                {
+                    g.angle -= ((yaw * 60) / 360);
+                }
             }
-            if (g[i].mount.front == 0)
-            {
-                g[i].angle = ((pitch * 60) / 360);
-            }
-        }
-    }
-
-    if (yaw > 0) // Veer Right
-    {
-        for (int i = 0; i < gnum; i++)
-        {
-            if (g[i].mount.left == 1)
-            {
-                g[i].angle = ((yaw * 60) / 360);
-            }
-            if (g[i].mount.left == 0)
-            {
-                g[i].angle = -1 * ((yaw * 60) / 360);
-            }
-        }
-    }
-
-    if (yaw < 0) // Veer Left
-    {
-        for (int i = 0; i < gnum; i++)
-        {
-            if (g[i].mount.left == 1)
-            {
-                g[i].angle = -1 * ((yaw * 60) / 360);
-            }
-            if (g[i].mount.front == 0)
-            {
-                g[i].angle = ((yaw * 60) / 360);
+            else if (yaw < 0)
+            { // Veer Left
+                if (g.mount.left == 1)
+                {
+                    g.angle -= ((yaw * 60) / 360);
+                }
+                else
+                {
+                    g.angle += ((yaw * 60) / 360);
+                }
             }
         }
     }
