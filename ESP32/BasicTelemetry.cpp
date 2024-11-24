be1f23c385a2fc7c9b0fd96f1974dd04e623947d
Hamza-Bin-Aamir <u2023219@giki.edu.pk>
2024-11-24 08:22:33 +0500

    Resolve ESP compilation issues

--- ESP32/BasicTelemetry.cpp
+++ ESP32/BasicTelemetry.cpp
@@ -1,27 +1,26 @@
-// WARNING: MAKE SURE TO USE THE ARDUINO IDE FOR THIS CODE TO WORK
-#include "MPU9250.h"
+#include "mpu9250.h"
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 
 // Set your access point network credentials
 const char* ssid = "Sentinel";
-const char* password = "KyunWorkNahinKarti";
+const char* password = "Password1";
 
 AsyncWebServer server(80);
 // an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
-MPU9250 IMU(Wire, 0x68);
+bfs::Mpu9250 IMU(&Wire, bfs::Mpu9250::I2C_ADDR_PRIM);
 int status;
 
 String getPitch(){
-  return String(IMU.getGyroX_rads());
+  return String(IMU.gyro_x_radps());
 }
 
 String getRoll(){
-  return String(IMU.getGyroY_rads());
+  return String(IMU.gyro_y_radps());
 }
 
 String getYaw(){
-  return String(IMU.getGyroZ_rads());
+  return String(IMU.gyro_z_radps());
 }
 
 
@@ -29,10 +28,8 @@ void setup() {
   // serial to display data
   Serial.begin(115200);
   while (!Serial) {}
-  
-
-  // start communication with IMU
-  status = IMU.begin();
+  Wire.begin();
+  Wire.setClock(400000);
   if (status < 0) {
     Serial.println("IMU initialization unsuccessful");
     Serial.println("Check IMU wiring or try cycling power");
@@ -60,27 +57,4 @@ void setup() {
 }
 
 void loop() {
-  // read the sensor
-  IMU.readSensor();
-  // display the data
-  Serial.print(IMU.getAccelX_mss(), 6);
-  Serial.print("\t");
-  Serial.print(IMU.getAccelY_mss(), 6);
-  Serial.print("\t");
-  Serial.print(IMU.getAccelZ_mss(), 6);
-  Serial.print("\t");
-  Serial.print(IMU.getGyroX_rads(), 6);
-  Serial.print("\t");
-  Serial.print(IMU.getGyroY_rads(), 6);
-  Serial.print("\t");
-  Serial.print(IMU.getGyroZ_rads(), 6);
-  Serial.print("\t");
-  Serial.print(IMU.getMagX_uT(), 6);
-  Serial.print("\t");
-  Serial.print(IMU.getMagY_uT(), 6);
-  Serial.print("\t");
-  Serial.print(IMU.getMagZ_uT(), 6);
-  Serial.print("\t");
-  Serial.println(IMU.getTemperature_C(), 6);
-  delay(100);
 }
