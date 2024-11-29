0e7cb06864cc0917c06f1aa5fbfe6e4c606b1d5d
AzeemLiaqat <azeemliaqat78@gmail.com>
2024-11-29 22:26:03 +0500

    utility functions and constructor

--- Security.hpp
+++ Security.hpp
@@ -9,4 +9,24 @@ class Security{
     private:
     string password;
 
+    public:
+    SecurityUtils(const string& initialPassword = "admin") {
+        password = initialPassword;
+    }
+    
+    // Change password
+    void setPassword(const string& newPassword) {
+        if (newPassword.empty()) {
+            throw runtime_error("Password cannot be empty");
+        }
+        password = newPassword;
+    }
+
+     bool checkPassword(const string& inputPassword) {
+        if(password == inputPassword)
+            return true;
+        else
+            return false;
+    }
+    
 }
