785cc8f2c05fe18322ece4fe504419e16c96ede4
SAreebZaheergit config --global user.name SAreebZaheer <sareebzaheer@gmail.com>
2024-12-10 20:37:00 +0500

    added enqueue() and dequeue()->ControlLoop.hpp

--- ControlLoop.hpp
+++ ControlLoop.hpp
@@ -27,4 +27,48 @@ public:
         rear = nullptr;
     }
 
+    // Check if the queue is empty
+    bool isEmpty() const
+    {
+        return front == nullptr;
+    }
+
+    // Enqueue: Add an element to the rear of the queue
+    void enqueue(string value)
+    {
+        Node *newNode = new Node(value);
+
+        if (isEmpty())
+        {
+            front = rear = newNode;
+        }
+        else
+        {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+
+    // Dequeue: Remove and return the element from the front of the queue
+    string dequeue()
+    {
+        if (isEmpty())
+        {
+            throw std::runtime_error("Queue is empty");
+        }
+
+        string data = front->data;
+        Node *temp = front;
+        front = front->next;
+
+        if (front == nullptr)
+        {
+            rear = nullptr; // If front becomes null, rear also becomes null
+        }
+
+        delete temp;
+        return data;
+    }
+
+
 };
